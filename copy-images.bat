1cec22955a8bf36fddb5e1a890e8526cc5438fca
Mark-maati <Mark-maati@users.noreply.github.com>
2026-04-01 22:50:29 +0300

    feat: update assets, project images, skills, and portfolio content

---
+++ copy-images.bat
@@ -0,0 +1,20 @@
+@echo off
+set "SRC=C:\Users\maati\.gemini\antigravity\brain\db68e8ca-fa4a-4317-a8b6-54b24f024264"
+set "DEST=c:\Users\maati\Downloads\New folder (24)\assets"
+
+copy "%SRC%\ai_content_engine_1774940003198.png" "%DEST%\ai_content.png" /Y
+copy "%SRC%\doc_intelligence_1774940019458.png" "%DEST%\doc_intel.png" /Y
+copy "%SRC%\collab_board_1774940035887.png" "%DEST%\collab_board.png" /Y
+copy "%SRC%\flowforge_1774940049861.png" "%DEST%\flowforge.png" /Y
+copy "%SRC%\high_perf_async_engine_1774940066021.png" "%DEST%\high_perf_async.png" /Y
+copy "%SRC%\juice_clone_1774940080999.png" "%DEST%\juice_clone.png" /Y
+copy "%SRC%\profile_hub_1774940102005.png" "%DEST%\profile_hub.png" /Y
+copy "%SRC%\marketplace_rpg_1774940119551.png" "%DEST%\marketplace.png" /Y
+copy "%SRC%\multi_tenant_saas_1774940136820.png" "%DEST%\multi_tenant.png" /Y
+copy "%SRC%\new_folder_24_maze_1774940153864.png" "%DEST%\new_folder_24.png" /Y
+copy "%SRC%\realtime_data_cache_1774940171190.png" "%DEST%\realtime_cache.png" /Y
+copy "%SRC%\secure_auth_boss_1774940189469.png" "%DEST%\secure_auth.png" /Y
+
+echo.
+echo 12 unique retro-gaming project images copied successfully! You can now refresh your browser.
+pause
